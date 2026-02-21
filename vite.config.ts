aaa671ca2e458933f6bbac190dd360c218e00ef0
Extinctable <massimo02caruso@gmail.com>
2026-02-21 11:37:58 -0500

    dist/ folder and updates to vite and ts config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 
 export default defineConfig({
 	plugins: [react()],
+	base: '/Health-Companion-Super-App/',
+	build: {
+		outDir: 'dist'
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
